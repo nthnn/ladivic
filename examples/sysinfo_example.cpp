c602c43b165a62b7fac0217abf663a431fd6d33c
nthnn <nathanneisip@gmail.com>
2024-01-28 02:46:43 +0800

    ldvc_cpu_cores() example usage.

--- examples/sysinfo_example.cpp
+++ examples/sysinfo_example.cpp
@@ -4,6 +4,7 @@
 
 i32 main() {
     std::cout << "CPU Information: " << ldvc_cpu_info() << std::endl;
+    std::cout << "Number of CPU cores: " << ldvc_cpu_cores() << std::endl;
     std::cout << "Total Memory: " << ldvc_total_memory() / 1024 / 1024 << " MBytes" << std::endl;
     std::cout << "Disk Space Available: " << ldvc_disk_space() << " bytes" << std::endl;
 
